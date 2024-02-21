4e1f23bcdc977a10a09f6b925a672cedf799cc58
eflaatten <elliotflaatten@live.com>
2024-02-20 22:04:04 -0600

    init todo-list. complete

---
+++ script.js
@@ -0,0 +1,38 @@
+let arrayOfTodos = [
+  {
+  "userId": 14,
+  "id": 1,
+  "title": "delectus aut autem",
+  "completed": false
+},
+{
+    "userId": 20,
+    "id": 2,
+    "title": "delectus aut autem",
+    "completed": false
+}]
+
+const fetchTodos = () => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then( (response) => response.json())
+    .then( (json) => arrayOfTodos = json)
+}
+  
+const logTodos = () => {
+    console.log(arrayOfTodos)
+}
+  
+const populateTodos = () => {
+
+  for(let i = 0; i < arrayOfTodos.length; i++){
+    const title = arrayOfTodos[i].title
+    const ol = document.getElementById('todo-list')
+    const li = document.createElement("li")
+    const liContent = document.createTextNode(title)
+    
+    console.log('title: ', title)
+    ol.appendChild(li)
+    li.append(liContent)
+  }
+
+}
